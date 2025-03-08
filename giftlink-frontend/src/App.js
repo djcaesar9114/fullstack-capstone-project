700bccbfd49ebf8bbb31e37fe633e62fc8e0dbae
djcaesar9114 <the_dj_caesar@hotmail.com>
2025-03-08 16:19:36 -0500

    Search Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-    import DetailsPage from './components/DetailsPage/DetailsPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -18,6 +19,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/search" element={<SearchPage/>} />
         </Routes>
         </>
   );
